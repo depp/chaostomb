485b885a5a399028705354e9e5c857a540ed3524
Dietrich Epp <depp@zdome.net>
2015-04-17 19:56:32 -0700

    Draw player on screen

--- src/main.js
+++ src/main.js
@@ -23,26 +23,16 @@ var player, platforms;
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.add.sprite(0, 0, 'sky');
-	platforms = game.add.group();
-	platforms.enableBody = true;
-	var ground = platforms.create(0, game.world.height - 64, 'platform');
-	ground.scale.setTo(2, 2);
-	ground.body.immovable = true;
-	var ledge = platforms.create(400, 400, 'platform');
-	ledge.body.immovable = true;
-	ledge = platforms.create(-150, 250, 'platform');
-	ledge.body.immovable = true;
 
-	player = game.add.sprite(32, game.world.height - 150, 'dude');
+	player = game.add.sprite(32, game.world.height - 150, 'player');
 	game.physics.arcade.enable(player);
 
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
 
-	player.animations.add('left', [0, 1, 2, 3], 10, true);
-	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	// player.animations.add('left', [0, 1, 2, 3], 10, true);
+	// player.animations.add('right', [5, 6, 7, 8], 10, true);
 }
 
 function update() {
