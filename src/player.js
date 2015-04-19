599f968dc7297cf2bd6d30e362bfc6f3e015d703
Dietrich Epp <depp@zdome.net>
2015-04-18 23:18:44 -0700

    Change Player prototype syntax

--- src/player.js
+++ src/player.js
@@ -22,96 +22,94 @@ function Player(level, obj) {
 	this.hearts = [];
 }
 
-Player.prototype = {
-	spawn: function(obj) {
-		this.sprite.reset(obj.x + obj.width / 2, obj.y + obj.height / 2);
-		var w;
-		for (w in weapons) {
-			this.addWeapon(w);
-		}
-		this.setHearts(7);
-	},
+Player.prototype.spawn = function(obj) {
+	this.sprite.reset(obj.x + obj.width / 2, obj.y + obj.height / 2);
+	var w;
+	for (w in weapons) {
+		this.addWeapon(w);
+	}
+	this.setHearts(7);
+};
 
-	update: function() {
-		var input = this.level.gInput;
-		var xdrive = 0, ydrive = 0, fire = false;
-		if (input.left.isDown) {
-			xdrive += -1;
-		}
-		if (input.right.isDown) {
-			xdrive += +1;
-		}
-		if (input.up.isDown) {
-			ydrive += -1;
-		}
-		if (input.down.isDown) {
-			ydrive += +1;
-		}
-		if (input.fire.isDown) {
-			fire = true;
-		}
+Player.prototype.update = function() {
+	var input = this.level.gInput;
+	var xdrive = 0, ydrive = 0, fire = false;
+	if (input.left.isDown) {
+		xdrive += -1;
+	}
+	if (input.right.isDown) {
+		xdrive += +1;
+	}
+	if (input.up.isDown) {
+		ydrive += -1;
+	}
+	if (input.down.isDown) {
+		ydrive += +1;
+	}
+	if (input.fire.isDown) {
+		fire = true;
+	}
 
-		game.physics.arcade.collide(this.sprite, this.level.gTiles);
-		this.mover.update(xdrive, ydrive);
+	game.physics.arcade.collide(this.sprite, this.level.gTiles);
+	this.mover.update(xdrive, ydrive);
 
-		if (fire && !this.fireDown) {
-			var sprite = this.sprite;
-			this.level.gShots.spawn(
-				Math.random() > 0.5 ? 'Bolt' : 'Eye',
-				sprite.x, sprite.y, this.mover.direction, 0);
-		}
-		this.fireDown = fire;
-	},
+	if (fire && !this.fireDown) {
+		var sprite = this.sprite;
+		this.level.gShots.spawn(
+			Math.random() > 0.5 ? 'Bolt' : 'Eye',
+			sprite.x, sprite.y, this.mover.direction, 0);
+	}
+	this.fireDown = fire;
+};
 
-	// Add a weapon to the player's inventory.
-	// Returns true if succesful, false otherwise.
-	addWeapon: function(weapon) {
-		var i;
-		for (i = 0; i < this.weapons.length; i++) {
-			if (this.weapons[i].name === weapon) {
-				return false;
-			}
-		}
-		var info = weapons[weapon];
-		if (!info) {
-			console.error('Unknown weapon:', weapon);
+// Add a weapon to the player's inventory.
+// Returns true if succesful, false otherwise.
+Player.prototype.addWeapon = function(weapon) {
+	var i;
+	for (i = 0; i < this.weapons.length; i++) {
+		if (this.weapons[i].name === weapon) {
 			return false;
 		}
-		var idx = this.weapons.length;
-		var rowlength = 7;
-		var x, y;
-		x = idx % rowlength;
-		y = Math.floor(idx / rowlength);
-		var size = 48, margin = 4;
-		var sprite = this.level.gUi.create(
-			size / 2 + margin + (margin + size) * x,
-			size / 2 + margin + (margin + size) * y,
-			'icons', 0);
-		sprite.anchor.setTo(0.5, 0.5);
-		if (this.weapons.length === 0) {
-			info.setIcon(sprite, true);
-			this.weapon = 0;
-		} else {
-			info.setIcon(sprite, false);
-		}
-		this.weapons.push({
-			name: weapon,
-			sprite: sprite
-		});
-	},
+	}
+	var info = weapons[weapon];
+	if (!info) {
+		console.error('Unknown weapon:', weapon);
+		return false;
+	}
+	var idx = this.weapons.length;
+	var rowlength = 7;
+	var x, y;
+	x = idx % rowlength;
+	y = Math.floor(idx / rowlength);
+	var size = 48, margin = 4;
+	var sprite = this.level.gUi.create(
+		size / 2 + margin + (margin + size) * x,
+		size / 2 + margin + (margin + size) * y,
+		'icons', 0);
+	sprite.anchor.setTo(0.5, 0.5);
+	if (this.weapons.length === 0) {
+		info.setIcon(sprite, true);
+		this.weapon = 0;
+	} else {
+		info.setIcon(sprite, false);
+	}
+	this.weapons.push({
+		name: weapon,
+		sprite: sprite
+	});
+},
 
-	setHearts: function(count) {
-		var i;
-		var size = 48, margin = 4;
-		for (i = this.hearts.length; i < count; i++) {
-			var sprite = this.level.gUi.create(
-				params.WIDTH - (size / 2 + margin + (margin + size) * i),
-				size / 2 + margin,
-				'hearts', 0);
-			sprite.anchor.setTo(0.5, 0.5);
-			this.hearts.push(sprite);
-		}
-	},
+Player.prototype.setHearts = function(count) {
+	var i;
+	var size = 48, margin = 4;
+	for (i = this.hearts.length; i < count; i++) {
+		var sprite = this.level.gUi.create(
+			params.WIDTH - (size / 2 + margin + (margin + size) * i),
+			size / 2 + margin,
+			'hearts', 0);
+		sprite.anchor.setTo(0.5, 0.5);
+		this.hearts.push(sprite);
+	}
 };
 
 module.exports = {
